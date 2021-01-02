4c54085785b508d4160a471eb7814955259433c0
NSCabezon <nscabezon@gmail.com>
2021-01-02 23:36:11 +0100

    fix convert to mp4

--- scripts/convert-video.py
+++ scripts/convert-video.py
@@ -19,17 +19,19 @@ else:
 		print("Output folder found")
 
 	i=START_INDEX
-	while(i <= END_INDEX):
-		input_file =  folder_root + videos_folder + "video%05d.h264" % i
+
+	dirs = os.listdir( path )
+	for file in dirs:
+		input_file = folder_root + videos_folder + "video%05d.h264" % i
 		output_file = folder_root + output_folder + "video%05d.mp4" % i
 		i = i + 1
 
-		print("Checking for existance of:  <<" + input_file + ">>")
+		print("Checking for existance of:  <<" + file + ">>")
 
 
-		if os.path.exists(input_file):
+		if os.path.exists(file):
 			print("Converting: " + input_file)
-			conversion_command = "MP4Box -add " + input_file + " " + output_file
+			conversion_command = "MP4Box -add " + input_file + " " + file + ".mp4"
 			print conversion_command
 			os.system(conversion_command)
 	print("Conversion complete.")
