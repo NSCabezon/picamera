ceb57235a52247340584b87bb111460649b56ea2
NSCabezon <nscabezon@gmail.com>
2021-01-12 17:07:57 +0100

    duration changed

--- dashcam.py
+++ dashcam.py
@@ -1,11 +1,11 @@
 import picamera
+from picamera import Color
 import os
 import time
 import json
 import itertools
 import constant
 import datetime
-from picamera import Color
 import checks
 
 def record():	
@@ -19,12 +19,10 @@ def record():
 		file_name = constant.videos_path + timestamp + ".h264"
 		
 		print('Recording to: ' + file_name)
-		
-		timeout = time.time() + constant.DURATION_SECS
 
 		camera.start_recording(file_name, quality = 20)
 
-		time.sleep(30)
+		time.sleep(constant.DURATION_SECS)
 		camera.stop_recording()
 		time.sleep(3)
 
