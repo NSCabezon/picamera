ce48c31d71d5163841dd0eb1bd6495edd7c4cb4d
NSCabezon <nscabezon@gmail.com>
2021-01-12 16:05:42 +0100

    remove duplicated /

--- convert-video.py
+++ convert-video.py
@@ -4,8 +4,8 @@ import os
 dirs = os.listdir(constant.videos_path)
 
 for file in dirs:
-	input_file = constant.videos_path + "/" + file
-	output_file = constant.output_path + "/" + file
+	input_file = constant.videos_path + file
+	output_file = constant.output_path + file
 	
 	print("Converting: " + file)
 	conversion_command = "MP4Box -add " + input_file + " " + output_file + ".mp4"
