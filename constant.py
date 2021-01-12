5e30d01bf89be7e31236a583c35c0132544c3670
NSCabezon <nscabezon@gmail.com>
2021-01-12 13:33:57 +0100

    moar changes

--- constant.py
+++ constant.py
@@ -1,6 +1,10 @@
+import os
+
 folder_root = "/home/pi/"
 videos_folder = "videos/"
 output_folder = "output/"
 
 videos_path = folder_root + videos_folder
 output_path = folder_root + output_folder
+
+dirs = os.listdir(constant.videos_path)
--- convert-video.py
+++ convert-video.py
@@ -2,23 +2,15 @@ import os
 import constant
 
 dirs = os.listdir(constant.videos_path)
-print(dirs)
-dirs.sort
-print(dirs)
 
 for file in dirs:
-	input_file = videos_path + "/" + file
-	output_file = output_path + "/" + file
-		
-	print("Checking for existance of:  <<" + file + ">>")
-
-	if os.path.exists(input_file):
-		print("Converting: " + input_file)
-		conversion_command = "MP4Box -add " + input_file + " " + output_file + ".mp4"
-		print conversion_command
-		os.system(conversion_command)
-	else: 
-		print("File <" + input_file + "> does not exist")
+	input_file = constant.videos_path + "/" + file
+	output_file = constant.output_path + "/" + file
+	
+	print("Converting: " + file)
+	conversion_command = "MP4Box -add " + input_file + " " + output_file + ".mp4"
+	print conversion_command
+	os.system(conversion_command)
 
 print("Conversion complete.")
 	
