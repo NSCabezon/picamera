d66bb3b26ccad1928c745c36379f46d3ae89d9a6
NSCabezon <nscabezon@gmail.com>
2021-01-15 11:54:43 +0100

    prompt in main

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ import checks
 import constant
 import convertVideo
 
-count = 0
 
-while (checks.check_space() < constant.SPACE_LIMIT and count < 3):
-	dashcam.record()
-	count += 1
+def record():
+	count = 0
 
-if checks.check_space() < constant.SPACE_LIMIT:
-	convertVideo.convertVideos()
+	while (checks.check_space() < constant.SPACE_LIMIT and count < 3):
+		dashcam.record()
+		count += 1
+
+def convert():
+	if checks.check_space() < constant.SPACE_LIMIT:
+		convertVideo.convertVideos()
+
+print("What do you want to do?")
+print("1 record in chunks of 10 min")
+print("2 manually record non converted videos and delete the original")
+print("3 both")
+
+val = input("Enter your value: ")
+if val == 1:
+	record()
+elif val == 2:
+	convert()
+elif val == 3: 
+	record()
+	convert()
+ 
