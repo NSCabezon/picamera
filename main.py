f2672df9a42216f1d70bcc83687ad6a1bd986309
NSCabezon <nscabezon@gmail.com>
2021-01-19 09:50:43 +0100

    move servo with numbers

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import constant
 import convertVideo
 
 from gpiozero import Servo
-
+import time as sleep
 
 def record():
 	count = 0
@@ -20,7 +20,12 @@ def convert():
 def moveServo():
 	print("Trying to move servo at GPIO 14")
 	servo = Servo(14)
-	servo.max()
+	servo.value = -1
+    sleep(2)
+	servo.value = 0
+    sleep(2)
+	servo.value = 1
+    sleep(2)
 
 print("What do you want to do?")
 print("1 record in chunks of 10 min")
